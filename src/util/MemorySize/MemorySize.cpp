0d56920b8c2433886a0a2883ab4a377195ee3129
ZamirCS-369 <z.caci@web.de>
2026-02-10 12:10:41 +0100

    Update MemorySize.cpp

--- src/util/MemorySize/MemorySize.cpp
+++ src/util/MemorySize/MemorySize.cpp
@@ -88,7 +88,7 @@ MemorySize MemorySize::parse(std::string_view str) {
     // `std::from_chars` which is currently not supported by the standard
     // library used by our macOS build.
     double amount;
-    absl::from_chars(amountString.data(), amountString.data() + .size(), amount);
+    absl::from_chars(amountString.data(), amountString.data() + amountString.size(), amount);
     auto unitString = matcher.get<unitGroup>().to_view();
     switch (std::tolower(unitString.at(0))) {
       case 'b':
