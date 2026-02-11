a1f4405d8c0f6418e6a5ec60487ae05d91141a51
ZamirCS-369 <z.caci@web.de>
2026-02-11 16:30:01 +0100

    Update StringUtils.h

--- src/util/StringUtils.h
+++ src/util/StringUtils.h
@@ -162,9 +162,19 @@ std::string insertThousandSeparator(const std::string_view str,
 inline QL_CONSTEXPR bool constantTimeEquals(std::string_view view1,
                                             std::string_view view2) {
   template <class charT>
-  struct my_char_traits{
+  struct my_char_traits {
+      using char_type  = char;
+      using int_type   = int;
+      using off_type   = streamoff;
+      using pos_type   = streampos;
+      using state_type = mbstate_t;
+      
       static size_t length(const char_type* c) {
-          return c.std::length();
+          int length = 0;
+          while (c[length] != '\0') {
+              length++;
+          }
+          return length;
       }
   };
   using byte_view = std::basic_string_view<volatile std::byte, my_char_traits<char>>;
